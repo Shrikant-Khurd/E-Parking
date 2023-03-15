3d6be45c4927fa5cfa4d8d6b54e78ca0664a010b
Shrikant Suresh Khurd <shrikantkh@cybage.com>
2023-03-15 15:20:25 +0530

    removed unused statements

--- src/main/java/com/epark/controller/AdminController.java
+++ src/main/java/com/epark/controller/AdminController.java
@@ -1,24 +1,20 @@
 package com.epark.controller;
 
-import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.epark.model.UserRole;
 import com.epark.model.Users;
 import com.epark.service.UserService;
-import com.epark.model.UserRole;
 
 @CrossOrigin("http://localhost:4200")
 @RestController
@@ -26,9 +22,6 @@ import com.epark.model.UserRole;
 public class AdminController {
 	@Autowired
 	private UserService userService;
-
-//	@Autowired
-//	private UserRepository userRepository;
 
 	@GetMapping("/get-all-users")
 	public ResponseEntity<List<Users>> getAllUsers() {
